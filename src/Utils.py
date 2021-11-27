f6650db317883b70f57f65fc4196c09c820e0bcf
Guillaume Aubert <me@aubertg.com>
2021-11-27 02:13:59 -0800

    Modularize main function

---
+++ src/Utils.py
@@ -0,0 +1,33 @@
+import socketpool
+import wifi
+
+
+class Utils:
+
+    def is_number(value):
+        return isinstance(value, (float, int))
+    
+    def get_secrets():
+        try:
+            from secrets import secrets
+        except ImportError:
+            print("Failed to import secrets.py")
+            raise
+        
+        return secrets
+
+    def connect_to_wifi(ssid, password):
+        try:
+            print(f'Connecting to {ssid}')
+            wifi.radio.connect(ssid, password)
+            print(f'Connected to {ssid}; IPv4 address = {wifi.radio.ipv4_address}')
+        except:
+            print("Error connecting to WiFi")
+            raise
+
+    def get_socket(interface, port):
+        pool = socketpool.SocketPool(wifi.radio)
+        socket = pool.socket()
+        socket.bind([interface, port])
+        socket.settimeout(1)
+        socket.listen(1)
