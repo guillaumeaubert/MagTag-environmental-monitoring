1adcc8383011b3244f73f8fc32d11cde91b9071a
Guillaume Aubert <me@aubertg.com>
2021-11-27 00:21:19 -0800

    Refactor PM AQI code

--- code.py
+++ code.py
@@ -14,6 +14,7 @@ from adafruit_pm25.i2c import PM25_I2C
 from digitalio import DigitalInOut, Direction, Pull
 
 from src.Display import Display
+from src.ParticulateMatter import ParticulateMatter
 
 
 # ----- CONFIGURATION VARIABLES -----
@@ -104,57 +105,6 @@ display = Display()
 magtag = MagTag()
 magtag.peripherals.neopixel_disable = False
 #magtag.peripherals.neopixels.fill((255, 255, 0))
-
-
-# ----- PM AQI CALCULATIONS -----
-# From https://aqs.epa.gov/aqsweb/documents/codetables/aqi_breakpoints.html
-
-def get_linear_value(aqi_high, aqi_low, concentration_high, concentration_low, concentration):
-    aqi = ((concentration - concentration_low) / (concentration_high -
-           concentration_low)) * (aqi_high - aqi_low) + aqi_low
-    return round(aqi)
-
-
-def calculate_pm25_aqi(concentration):
-    concentration = (round(10*concentration))/10
-
-    if 0.0 <= concentration < 12.1:
-        return get_linear_value(50, 0, 12, 0, concentration)
-    if 12.1 <= concentration < 35.5:
-        return get_linear_value(100, 51, 35.4, 12.1, concentration)
-    if 35.5 <= concentration < 55.5:
-        return get_linear_value(150, 101, 55.4, 35.5, concentration)
-    if 55.5 <= concentration < 150.5:
-        return get_linear_value(200, 151, 150.4, 55.5, concentration)
-    if 150.5 <= concentration < 250.5:
-        return get_linear_value(300, 201, 250.4, 150.5, concentration)
-    if 250.5 <= concentration < 350.5:
-        return get_linear_value(400, 301, 350.4, 250.5, concentration)
-    if 350.5 <= concentration < 500.5:
-        return get_linear_value(500, 401, 500.4, 350.5, concentration)
-
-    return "Out of Range"
-
-
-def calculate_pm10_aqi(concentration):
-    concentration = round(concentration)
-
-    if 0 <= concentration < 55:
-        return get_linear_value(50, 0, 54, 0, concentration)
-    if 55 <= concentration < 155:
-        return get_linear_value(100, 51, 154, 55, concentration)
-    if 155 <= concentration < 255:
-        return get_linear_value(150, 101, 254, 155, concentration)
-    if 255 <= concentration < 355:
-        return get_linear_value(200, 151, 354, 255, concentration)
-    if 355 <= concentration < 425:
-        return get_linear_value(300, 201, 424, 355, concentration)
-    if 425 <= concentration < 505:
-        return get_linear_value(400, 301, 504, 425, concentration)
-    if 505 <= concentration < 605:
-        return get_linear_value(500, 401, 604, 505, concentration)
-
-    return "Out of Range"
 
 
 # ----- BME680 SENSOR -----
@@ -279,8 +229,8 @@ def get_data(request):
                     "10um_per_0.1l": aqdata["particles 100um"],
                 },
                 "AQI": {
-                    "PM_2.5": calculate_pm25_aqi(aqdata["pm25 env"]),
-                    "PM_10": calculate_pm10_aqi(aqdata["pm100 env"])
+                    "PM_2.5": ParticulateMatter.calculate_pm25_aqi(aqdata["pm25 env"]),
+                    "PM_10": ParticulateMatter.calculate_pm10_aqi(aqdata["pm100 env"])
                 }
             },
             "magtag": {
@@ -329,7 +279,7 @@ def get_display_data():
     pm25_aqi = ""
     try:
         aqdata = pm25_sensor.read()
-        pm25_aqi = calculate_pm25_aqi(aqdata["pm25 env"])
+        pm25_aqi = ParticulateMatter.calculate_pm25_aqi(aqdata["pm25 env"])
     except:
         print("Failed to read PM 2.5 data")
     pm25_aqi = f"{pm25_aqi:d}" if is_number(pm25_aqi) else "(?)"
